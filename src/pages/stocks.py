3ddf70681f8a28662b79965beef013aa08166ae8
MoeMaki <owenngo72@gmail.com>
2024-09-27 15:42:45 +0800

    Update stocks.py
    -made the stocks button softcoded

--- src/pages/stocks.py
+++ src/pages/stocks.py
@@ -5,30 +5,21 @@ import dash_bootstrap_components as dbc
 ##stocks page
 dash.register_page(__name__, )
 
+stocks=["a","b"]
+
 layout = html.Div([
     html.H1('This is our Stocks page'),
     html.Div('This is our Stocks page content.'),
     html.A(
-        
         children=[
-            dbc.Button("AAPL", color="primary", class_name="mx-1")
+            html.A(
+            dbc.Button(f"{stock}", color="primary", class_name="mx-1"),
+            href=f"/stocks/{stock}"
+            )for stock in stocks
         ],
-        href="/stocks/AAPL"
-    ),
-      html.A(
-        
-        children=[
-            dbc.Button("AAPL", color="primary", class_name="mx-1")
-        ],
-        href="/stocks/AAPL"
-    ),
-      html.A(
-        
-        children=[
-            dbc.Button("AAPL", color="primary", class_name="mx-1")
-        ],
-        href="/stocks/AAPL"
-    ),
+    )
+
+    
 
   
 ])
