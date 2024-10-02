6dc26cce8840a69fffd74d4234129829f2caaa9b
derickiscool <60564377+derickiscool@users.noreply.github.com>
2024-10-02 11:27:19 +0800

    Added base layout header

---
+++ src/components/stock/base/header_layout.py
@@ -0,0 +1,59 @@
+
+from dash import html, callback, Output,  Input, dcc,ctx, ALL
+
+@callback(Output("stock-difference-header", "style"), Input("header-store", "data"))
+def difference_color(data):
+    if (data["difference"] >= 0):
+        return {"color" : "green"}
+    else:
+        return {"color" : "red"}
+    
+    
+@callback ([Output("stock-title", "children"),Output("stock-close-header", "children"), Output("stock-difference-header", "children")], Input("header-store", "data"))
+def assign_headers(data):
+    print("Fetching header data: ", data)
+    stock_name = data["stock_name"]
+    stock_id = data["stock_id"]
+    close_price = data["close"]
+    stock_difference =  data["difference"]
+    if (stock_difference > 0):
+        stock_difference = f"+{stock_difference:.2f}"
+    else:
+        stock_difference = f"{stock_difference:.2f}"
+    stock_difference_percentage = data["percentage_difference"]
+    stock_title = f"{stock_name} ({stock_id})"
+    difference = f"{stock_difference} ({stock_difference_percentage:.2f}%)"
+    return stock_title, close_price, difference
+
+def header_layout():
+ 
+  
+
+    return (
+      
+        html.Div
+        (
+            
+            [
+                dcc.Store(id='header-store'),
+                html.H1([], className="text-xl text-mono font-bold", id="stock-title"),
+                html.Div
+                (
+                    [
+                    html.Div
+                    (
+                        [
+                        html.H2([], className="text-4xl text-mono font-bold" , id="stock-close-header"),
+                        html.P([], className="text-lg   text-mono", id="stock-difference-header")
+                        ],className="flex gap-2"
+                    ),
+                    html.P("At Close: Sep 27, 2024, 4:00PM" , className="text-md")
+                    
+                    ], 
+                ),
+                
+                
+                
+            ], className="flex flex-col gap-4"
+        )
+    )
--- src/components/stock/single_stock_base_layout.py
+++ src/components/stock/single_stock_base_layout.py
@@ -5,24 +5,59 @@ from dash import dcc
 import plotly.express as px
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
-
+from src.components.stock.base.header_layout import header_layout
 import yfinance as yf
 '''
 This file contains the base layout of all single stock pages. 
 '''
 
+#Whatever needs to be fetched, fetch and store in dcc store for other components to access
+@callback (Output("header-store", "data"), Input("url","pathname"))
+def fetch_layout_data(url : str):
+        ##Fetches Name and current close pricings -> for name price layout
+        def fetch_header_data(ticker):
+            
+            #To get the percentage increase and difference, get the previous close and subtract
+            def calculate_difference(close_price, prev_close):
+                difference = round(close_price - prev_close,2)
+                percentage_difference = round(difference/prev_close * 100,2)
+                return {"difference" : difference, "percentage_difference" : percentage_difference}
+            
+            company_name = ticker.info['longName']
+            close_price = ticker.info["currentPrice"]
+            prev_close = ticker.info["previousClose"]
+            difference_dic = calculate_difference(close_price, prev_close)
+            header_dic = {
+                "stock_name" : company_name, 
+                "stock_id" : stock_id, 
+                "close" : close_price
+                }
+            header_dic.update(difference_dic)
+            print(header_dic)
+        
+            return header_dic
+        ##Get stock ID based on the URL
+        stock_id = url.split("/")[2]
+        ##Current ticker
+        ticker = yf.Ticker(stock_id)
+        header = fetch_header_data(ticker=ticker)
+        return header
+    
+
+
+
+
 def stock_base_layout():
     '''
     This function will return the stock base layout used in all nested stock pages
     '''
-    
-    
     return (
         
         html.Div(
             [
                 dcc.Location(id="url"),
-                html.P("Base Layout")
+                header_layout()
+                
             ],
             
            
