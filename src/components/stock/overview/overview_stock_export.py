b05d0ccc77b140bf0df1d1a6b332a5c20d780d9e
Mars <60571771+xMarzu@users.noreply.github.com>
2024-10-09 23:25:33 +0800

    Create overview_stock_export.py
    logic for CSV exporting of stock data

---
+++ src/components/stock/overview/overview_stock_export.py
@@ -0,0 +1,21 @@
+import os
+import yfinance as yf
+
+# Download CSV file containing stock data into users download folder
+def downloadCSV(ticker,period):
+    try:
+        #Download stock data from yfinance for the specific stock ticker and period
+        data = yf.download(tickers=ticker,period = period)
+
+        #Set download path for user's download folder
+        download_folder = os.path.expanduser('~/Downloads')
+        csv_file_path = os.path.join(download_folder, f'{ticker}_data.csv')
+
+        #Save the downloaded data into a CSV file
+        data.to_csv(csv_file_path)
+
+        print(f"Data for {ticker} saved to {csv_file_path}")
+
+    except:
+        print(f'Error downloading data for {ticker}')
+
